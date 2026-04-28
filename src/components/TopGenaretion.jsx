f64f1a51bb055837693674b829b5d3c1b21f79c2
MDSOBUJMADBOR <sobujmadbor660@gmail.com>
2026-04-28 13:03:19 +0600

    added banner footer navbar topgenaration

---
+++ src/components/TopGenaretion.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TopGeneration = () => {
+          return (  
+          <div className='max-w-7xl mx-auto '>
+      <h1 className='text-2xl font-bold'>Top Generations</h1>  
+
+   
+
+          </div>
+          );
+};
+
+export default TopGeneration;
